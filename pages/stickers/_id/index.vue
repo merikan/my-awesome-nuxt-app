98115d555c9f24367e2832a775103029fbdd4502
Peter Merikan <peter@merikan.com>
2018-11-24 21:19:53 +0100

    load data from fake store

--- pages/stickers/_id/index.vue
+++ pages/stickers/_id/index.vue
@@ -1,13 +1,46 @@
 <template>
   <section class="single-sticker">
-    <h1>{{ $route.params.id }}</h1>
+    <h1>{{ sticker.title }}</h1>
     <div>
-      <img src="" alt="">
+      <img class="sticker-image" :src="sticker.thumbnail" :alt="sticker.title">
     </div>
-    <p>Sticker description</p>
+    <p>{{sticker.descriptionText}}</p>
   </section>
 </template>
 
+<script>
+  export default {
+    asyncData(context) {
+      // return axios.get()   for a real request
+      // hardcoded list of stickers
+      // fake async request, add a 1.5s delay
+      return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          resolve({
+            sticker: [
+              {
+                id: "1",
+                title: "Gopher Boxer",
+                previewText: "Keep on boxing!!",
+                descriptionText: "fmt u all. I'm the master of boxing",
+                thumbnail: "https://gopherswag.com/img/gophercises-box.png"
+              },
+              {
+                id: "2",
+                title: "Jump Rope Gopher",
+                previewText: "Keep on jumping!!",
+                descriptionText: "fmt u all. I'm the master of jumping",
+                thumbnail: "https://gopherswag.com/img/gophercises-jump.png"
+
+              }
+            ].find(el => el.id === context.params.id)
+          })
+        }, 1500)
+      })
+    }
+  }
+
+</script>
 <style>
 .single-sticker {
   display: flex;
@@ -16,5 +49,8 @@
   align-items: center;
   text-align: center;
   padding: 30px;
+}
+.sticker-image {
+  width: 100%;
 }
 </style>
--- pages/stickers/index.vue
+++ pages/stickers/index.vue
@@ -1,24 +1,47 @@
 <template>
   <section class="stickers">
     <Sticker
-      thumbnail= "https://gopherswag.com/img/gophercises-box.png"
-      title="Gopher Boxer"
-      id="1"
-      previewText="Keep on boxing!!"/>
-    <Sticker
-      thumbnail= "https://gopherswag.com/img/gophercises-jump.png"
-      title="Jump Rope Gopher"
-      id="2"
-      previewText="Keep on jumping!!"/>
+      v-for="sticker in stickers"
+      :key="sticker.id"
+      :thumbnail= "sticker.thumbnail"
+      :title="sticker.title"
+      :id="sticker.id"
+      :previewText="sticker.previewText"/>
   </section>
 </template>
 
 <script>
-  import Sticker from '@/components/Sticker'
+  import Sticker from '@/components/Sticker';
 
   export default {
     components: {
       Sticker
+    },
+    asyncData() {
+      // return axios.get()   for a real request
+      // hardcoded list of stickers
+      // fake async request, add a 1.5s delay
+      return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          resolve({
+            stickers: [
+              {
+                id: "1",
+                title: "Gopher Boxer",
+                previewText: "Keep on boxing!!",
+                thumbnail: "https://gopherswag.com/img/gophercises-box.png"
+              },
+              {
+                id: "2",
+                title: "Jump Rope Gopher",
+                previewText: "Keep on jumping!!",
+                thumbnail: "https://gopherswag.com/img/gophercises-jump.png"
+
+              }
+            ]
+          })
+        }, 1500)
+      })
     }
   }
 </script>
