c72793a3b6d9b8d8792cb4739eea96bf563b9703
Peter Merikan <peter@merikan.com>
2018-11-24 21:27:15 +0100

    add a header

---
+++ components/Header.vue
@@ -0,0 +1,37 @@
+<template>
+  <header>
+    <nav>
+      <ul>
+        <li><nuxt-link to="/">Starting page</nuxt-link></li>
+        <li><nuxt-link to="/stickers">Stickers</nuxt-link></li>
+        <li><nuxt-link to="/about">About</nuxt-link></li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<style scoped>
+  header {
+    width: 100%;
+    height: 30px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: #521751;
+  }
+  ul {
+    list-style: none;
+    padding: 0;
+    margin: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  li {
+    margin: 0 10px;
+  }
+  a {
+    text-decoration: none;
+    color: white;
+  }
+</style>
